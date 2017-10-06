6f2b11f465f07f162ae960bf73ad7422de8e0389
Dan <daniel.munozdev@gmail.com>
2017-10-06 14:34:39 -0700

    Add variables with values from form, and add them to spans in the story div

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 $('#form1').submit(function(event) {
+  let person1Input = $('input#person1').val();
+  let person2Input = $('input#person2').val();
+  let animalInput = $('input#animal').val();
+  let exclamationInput = $('input#exclamation').val();
+  let verbInput = $('input#verb').val();
+  let nounInput = $('input#noun').val();
 
+  $('.person1').text(person1Input);
+  $('.person2').text(person2Input);
+  $('.animal').text(animalInput);
+  $('.exclamation').text(exclamationInput);
+  $('.verb').text(verbInput);
+  $('.noun').text(nounInput);
 
   $('#story').slideDown();
   event.preventDefault();
