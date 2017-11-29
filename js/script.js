1eb30cd4c0a7dbe8e2f00a2388a6b928deed3386
dan <daniel.munozdev@gmail.com>
2017-11-28 20:14:58 -0800

    Refactor getting the value of the inputs, and then put it in the appropriate blank space using arrays and looping

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
+// user interface (or front-end) logic
 $('#form1').submit(function(event) {
-  let person1Input = $('input#person1').val();
-  let person2Input = $('input#person2').val();
-  let animalInput = $('input#animal').val();
-  let exclamationInput = $('input#exclamation').val();
-  let verbInput = $('input#verb').val();
-  let nounInput = $('input#noun').val();
-
-  $('.person1').text(person1Input);
-  $('.person2').text(person2Input);
-  $('.animal').text(animalInput);
-  $('.exclamation').text(exclamationInput);
-  $('.verb').text(verbInput);
-  $('.noun').text(nounInput);
-
+  // refactored code
+  let inputs = ['person1', 'person2', 'animal', 'exclamation', 'verb', 'noun'];
+  inputs.forEach(function(input) {
+    let userInput = $('input#' + input).val();
+    $('.' + input).text(userInput);
+  });
+  // old code
+  // let person1Input = $('input#person1').val();
+  // let person2Input = $('input#person2').val();
+  // let animalInput = $('input#animal').val();
+  // let exclamationInput = $('input#exclamation').val();
+  // let verbInput = $('input#verb').val();
+  // let nounInput = $('input#noun').val();
+  //
+  // $('.person1').text(person1Input);
+  // $('.person2').text(person2Input);
+  // $('.animal').text(animalInput);
+  // $('.exclamation').text(exclamationInput);
+  // $('.verb').text(verbInput);
+  // $('.noun').text(nounInput);
   $('#story').slideDown();
   event.preventDefault();
 });
